4d3a8b949ed724a2a60239b958f84bef03a444d1
vivanac <vedran.ivanac@gmail.com>
2013-07-11 21:09:06 +0200

    initial

---
+++ mysite/polls/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+class Poll(models.Model):
+	question = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date_published')
+
+	def __unicode__(self):
+		return self.question
+
+	def was_published_recently(self):
+		return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <  timezone.now()
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
+
+class Choice(models.Model):
+	poll = models.ForeignKey(Poll)
+	choice_text = models.CharField(max_length = 200)
+	votes = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.choice_text
